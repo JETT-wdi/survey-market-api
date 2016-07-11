6aaa83682f6bfcd06e95977ec09541d9699220b4
jacobhereford <jacobhereford@hotmail.com>
2016-07-11 15:27:50 -0400

    removed secret token
    removed token key

--- scripts/edit-survey.sh
+++ scripts/edit-survey.sh
@@ -9,7 +9,7 @@ curl --include --request PATCH http://localhost:3000/change-password/$ID \
 
 
   curl --include --request PATCH http://localhost:3000/surveys/5783e027e52da61d56deb05e \
-    --header "Authorization: Token token=jHkB1HHwXjDZCP8/zaCeklxHPQjViipBTt2iRzM/cgw=--+Tmb7D9zeQKel5vjAWEq7tJyrw7fhBUTZVMksG6v8Ok=" \
+    --header "Authorization: Token token= \
     --header "Content-Type: application/json" \
     --data '{
       "survey": {
