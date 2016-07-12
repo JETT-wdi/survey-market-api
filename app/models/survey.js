5ad7bd7d045ae8f88489b63d9afad5e101b8f8df
tina513 <dlnaz.ai@hotmail.com>
2016-07-12 15:16:48 -0400

    change schema and model to match front end

--- app/models/survey.js
+++ app/models/survey.js
@@ -3,37 +3,24 @@
 const mongoose = require('mongoose');
 
 const surveySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  questions: [{
-    query: {
-      type: String,
-      required: true,
-    },
-    answers: [{
-      option: {
+      title: {
         type: String,
-        required: true,
       },
-      numberOfVotes: {
-        type: Number,
-        required: true,
-        default: 0,
+      questions: [
+        [{ query: { type: String, }},
+         {option: { type: String, }, numberOfVotes: { type: Number, default: 0 } }
+        ]
+      ],
+      _owner: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+          required: true,
+        }
       },
-    }]
-  }],
+      {
+        timestamps: true,
+      });
 
-  _owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-}, {
-  timestamps: true,
-});
+    const Survey = mongoose.model('Survey', surveySchema);
 
-const Survey = mongoose.model('Survey', surveySchema);
-
-module.exports = Survey;
+    module.exports = Survey;
