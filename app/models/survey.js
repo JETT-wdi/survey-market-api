e59a09505533ce833bd7e4549b47d685b9a01b27
erinsul <erin_sullivan50@yahoo.com>
2016-07-13 14:47:08 -0400

    Add properly working virtual "numVotes" for answers
    Split question and answer schemas away from the main survey schema
    in order to add a virtual to count number of votes for an answer.
    It wasn't working before because it was a virtual property of ANSWERS
    not SURVEYS .

--- app/models/survey.js
+++ app/models/survey.js
@@ -1,25 +1,41 @@
 'use strict';
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+// const Question = require('./question.js');
+
+const answerSchema = new mongoose.Schema({
+  text: {
+    type: String,
+  },
+  votes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true},
+});
+
+answerSchema.virtual('numVotes').get(function () {
+  return this.votes.length;
+});
+
+const questionSchema = new mongoose.Schema({
+    query: {
+      type: String,
+      required: true,
+    },
+    answers: [answerSchema]
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true},
+});
+
+
 const surveySchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
     unique: true
   },
-  questions: [{
-    query: {
-      type: String,
-      required: true,
-    },
-    answers: [{
-      text: {
-        type: String,
-      },
-      votes: [{
-      }],
-    }]
-  }],
+  questions: [questionSchema],
   _owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
@@ -28,9 +44,6 @@ const surveySchema = new mongoose.Schema({
 }, {
   timestamps: true,
   toJSON: { virtuals: true},
-});
-surveySchema.virtual('questions.answers.numVotes').get(function () {
-  return this.questions.answers.votes.length;
 });
 
 surveySchema.plugin(uniqueValidator);
